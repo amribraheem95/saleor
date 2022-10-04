dfcb692932c0d3bf598ee95dd4b6de65f21da947
zedzior <piotr.zabieglik@gmail.com>
2022-10-04 12:26:31 +0200

    fix `draftOrderCreate` mutation to save `shipping_method_name`

--- saleor/graphql/order/mutations/order_update.py
+++ saleor/graphql/order/mutations/order_update.py
@@ -76,7 +76,7 @@ class OrderUpdate(DraftOrderCreate):
     @traced_atomic_transaction()
     def save(cls, info, instance, cleaned_input):
 
-        cls._save_addresses(info, instance, cleaned_input)
+        cls._save_addresses(instance, cleaned_input)
         if instance.user_email:
             user = User.objects.filter(email=instance.user_email).first()
             instance.user = user
